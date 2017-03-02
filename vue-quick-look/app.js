96588dedcf70020a6c4b38596ce1a1939871e5ef
Brett Nelson <BrettMN@outlook.com>
2017-03-01 23:31:39 -0600

    Vue.js: display task list

--- vue-quick-look/app.js
+++ vue-quick-look/app.js
@@ -1,6 +1,20 @@
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello WIPDeveloper.com!'
+    message: 'Hello WIPDeveloper.com!',
+    tasks: [
+      {
+        title: 'test task one',
+        complete: true,
+        description: 'this is a task that is complete'
+      },
+
+      {
+        title: 'test task two',
+        complete: false,
+        description: 'this is a task that is not complete'
+      }
+    ]
   }
 })
